9529777bb4f123b35075be7475e641f309d13a24
Abhishek Sharma <dhanvantriabhi@gmail.com>
2018-05-03 23:07:44 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TreeViewComponent } from './components/tree-view/tree-view.component';
+import { ItemFormComponent } from './components/item-form/item-form.component';
+import { MapComponent } from './components/map/map.component';
+// Import the Animations module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Import the ButtonsModule
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { SplitterModule } from '@progress/kendo-angular-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TreeViewComponent,
+    ItemFormComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    ButtonsModule,
+    SplitterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
